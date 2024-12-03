530b8d93074a43589a2e6b0e55279e1a863e6adb
amanisheikh1 <amani@loopequity.co>
2024-12-03 13:13:50 +0000

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Endpoint for handling form submission
-app.post("/submit", async (req, res) => {
+app.post("/", async (req, res) => {
     const { email, firstName } = req.body;
 
     try {
